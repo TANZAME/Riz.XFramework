d7ff4aff6105103633d57399bfc8fd99483d1156
TANZAME <tian_naixiang@sina.com>
2019-07-18 11:28:42 +0800

    -

--- .nuget/package.bat
+++ .nuget/package.bat
@@ -8,33 +8,49 @@ cd .nuget
 :: 打包 TZM.XFramework -Build
 echo pack TZM.XFramework
 nuget pack %startup_dir%\net45\TZM.XFramework\TZM.XFramework.csproj
+echo=
+echo=
 
 :: 打包 TZM.XFramework.MySql
 echo pack TZM.XFramework.MySql
 nuget pack %startup_dir%\net45\TZM.XFramework.MySql\TZM.XFramework.MySql.csproj
+echo=
+echo=
 
 :: 打包 TZM.XFramework.Oracle
 echo pack TZM.XFramework.Oracle
 nuget pack %startup_dir%\net45\TZM.XFramework.Oracle\TZM.XFramework.Oracle.csproj
+echo=
+echo=
 
 :: 打包 TZM.XFramework.Postgre
 echo pack TZM.XFramework.Postgre
 nuget pack %startup_dir%\net45\TZM.XFramework.Postgre\TZM.XFramework.Postgre.csproj
+echo=
+echo=
 
 :: 打包 TZM.XFrameworkCore
 echo pack TZM.XFrameworkCore
 dotnet pack --no-build --output %startup_dir%\.nuget\ %startup_dir%\netcore\TZM.XFrameworkCore\TZM.XFrameworkCore.csproj
+echo=
+echo=
 
 :: 打包 TZM.XFrameworkCore.MySql
 echo pack TZM.XFrameworkCore.MySql
 dotnet pack --no-build --output %startup_dir%\.nuget\ %startup_dir%\netcore\TZM.XFrameworkCore.MySql\TZM.XFrameworkCore.MySql.csproj
+echo=
+echo=
 
 :: 打包 TZM.XFrameworkCore
 echo pack TZM.XFrameworkCore.Oracle
 dotnet pack --no-build --output %startup_dir%\.nuget\ %startup_dir%\netcore\TZM.XFrameworkCore.Oracle\TZM.XFrameworkCore.Oracle.csproj
+echo=
+echo=
 
 :: 打包 TZM.XFrameworkCore.Postgre
 echo pack TZM.XFrameworkCore.Postgre
 dotnet pack --no-build --output %startup_dir%\.nuget\ %startup_dir%\netcore\TZM.XFrameworkCore.Postgre\TZM.XFrameworkCore.Postgre.csproj
+echo=
+echo=
 
 pause
