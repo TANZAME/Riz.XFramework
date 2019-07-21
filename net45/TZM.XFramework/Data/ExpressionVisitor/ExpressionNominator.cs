29598a2e13e6ac6000a642234a0e390b6074a1bb
TANZAME <tian_naixiang@sina.com>
2019-07-21 09:37:42 +0800

    -

---
+++ net45/TZM.XFramework/Data/ExpressionVisitor/ExpressionNominator.cs
@@ -0,0 +1,64 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+
+namespace TZM.XFramework.Data
+{
+    public class ExpressionNominator : ExpressionVisitor
+    {
+        private readonly Func<Expression, bool> _canBeEvaluated;
+        private HashSet<Expression> _candidates;
+        private bool _cannotBeEvaluated;
+
+        public ExpressionNominator()
+          : this(new Func<Expression, bool>(ExpressionNominator.CanBeEvaluatedLocally))
+        {
+        }
+
+        public ExpressionNominator(Func<Expression, bool> fnCanBeEvaluated)
+        {
+            this._canBeEvaluated = fnCanBeEvaluated ?? new Func<Expression, bool>(ExpressionNominator.CanBeEvaluatedLocally);
+        }
+
+        private static bool CanBeEvaluatedLocally(Expression exp)
+        {
+            return exp.NodeType != ExpressionType.Parameter && exp.NodeType != ExpressionType.MemberInit && exp.NodeType != ExpressionType.New;
+        }
+
+        public HashSet<Expression> Nominate(Expression expression)
+        {
+            this._candidates = new HashSet<Expression>();
+            this.Visit(expression);
+            return this._candidates;
+        }
+
+        public override Expression Visit(Expression expression)
+        {
+            if (expression != null)
+            {
+                bool cannotBeEvaluated = this._cannotBeEvaluated;
+                this._cannotBeEvaluated = false;
+                base.Visit(expression);
+
+                if (!this._cannotBeEvaluated)
+                {
+                    if (this._canBeEvaluated(expression))
+                    {
+                        if (expression.NodeType != ExpressionType.Constant)
+                            this._candidates.Add(expression);
+                    }
+                    else
+                    {
+                        this._cannotBeEvaluated = true;
+                    }
+                }
+
+                this._cannotBeEvaluated = this._cannotBeEvaluated | cannotBeEvaluated;
+            }
+            return expression;
+        }
+    }
+
+
+}
