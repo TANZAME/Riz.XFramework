3751a6f010df9e7db63e8869dde4a9acb9f879b7
TANZAME <tian_naixiang@sina.com>
2019-07-22 19:50:31 +0800

    -

---
+++ net45/TZM.XFramework/Data/Genaration/ResolveScope.cs
@@ -0,0 +1,22 @@
+﻿
+using System.Data;
+using System.Collections.Generic;
+
+namespace TZM.XFramework.Data
+{
+    /// <summary>
+    /// 解析上下文
+    /// </summary>
+    public class ResolveScope
+    {
+        /// <summary>
+        /// 参数列表
+        /// </summary>
+        public List<IDbDataParameter> Parameters { get; set; }
+
+        /// <summary>
+        /// 已使用别名数量
+        /// </summary>
+        public int AliasTaked { get; set; }
+    }
+}
