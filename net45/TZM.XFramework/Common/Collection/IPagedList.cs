e5b9f1458a22abc6575882e68f503a0992b65389
TANZAME <tian_naixiang@sina.com>
2019-11-22 10:07:57 +0800

    pagelist fix

--- net45/TZM.XFramework/Common/Collection/IPagedList.cs
+++ net45/TZM.XFramework/Common/Collection/IPagedList.cs
@@ -9,7 +9,7 @@ namespace TZM.XFramework
         /// <summary>
         /// 当前页码
         /// </summary>
-        int PageIndex { get; }
+        int PageIndex { get; set; }
 
         /// <summary>
         /// 页长
@@ -17,26 +17,26 @@ namespace TZM.XFramework
         /// 注意：1024是一个特殊值，表示不分页查询所有
         /// </para>
         /// </summary>
-        int PageSize { get; }
+        int PageSize { get; set; }
 
         /// <summary>
         /// 记录总数
         /// </summary>
-        int RowCount { get; }
+        int RowCount { get; set; }
 
         /// <summary>
         /// 总页数
         /// </summary>
-        int Pages { get; }
+        int Pages { get; set; }
 
-        /// <summary>
-        /// 能否进行上一次查询
-        /// </summary>
-        bool HasPreviousPage { get; }
+        ///// <summary>
+        ///// 能否进行上一次查询
+        ///// </summary>
+        //bool HasPreviousPage { get; }
 
-        /// <summary>
-        /// 能否进行下一页查询
-        /// </summary>
-        bool HasNextPage { get; }
+        ///// <summary>
+        ///// 能否进行下一页查询
+        ///// </summary>
+        //bool HasNextPage { get; }
     }
 }
