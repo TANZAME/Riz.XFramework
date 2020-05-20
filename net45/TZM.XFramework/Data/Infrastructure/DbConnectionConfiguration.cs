43c1d38d2526533d4358ca42c9f99f5b1589bb3e
TANZAME <tian_naixiang@sina.com>
2020-05-20 19:50:40 +0800

    - master - slave - plan

---
+++ net45/TZM.XFramework/Data/Infrastructure/DbConnectionConfiguration.cs
@@ -0,0 +1,16 @@
+﻿
+namespace TZM.XFramework.Data
+{
+    /// <summary>
+    /// 数据库连接配置
+    /// </summary>
+    public class DbConnectionConfiguration
+    {
+        /// <summary>
+        /// 连接字符串
+        /// </summary>
+        public string ConnectionString { get; set; }
+
+
+    }
+}
