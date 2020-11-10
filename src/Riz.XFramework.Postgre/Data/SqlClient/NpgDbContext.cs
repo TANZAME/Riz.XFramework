c1d68c5ec197cd2b8ea156c608ff2f242f50e58c
TANZAME <tian_naixiang@sina.com>
2020-11-10 22:37:46 +0800

    -

---
+++ src/Riz.XFramework.Postgre/Data/SqlClient/NpgDbContext.cs
@@ -0,0 +1,51 @@
+﻿
+using Npgsql;
+
+namespace Riz.XFramework.Data.SqlClient
+{
+    /// <summary>
+    /// 数据上下文，框架的主入口点，非线程安全
+    /// </summary>
+    public class NpgDbContext : DbContextBase
+    {
+        /// <summary>
+        /// 大小写敏感，适用于建表时使用区分大小写的情况
+        /// </summary>
+        public bool CaseSensitive { get; set; }
+
+        /// <summary>
+        /// 查询语义提供者
+        /// </summary>
+        public override IDbQueryProvider Provider { get { return NpgDbQueryProvider.Instance; } }
+
+        /// <summary>
+        /// 初始化 <see cref="NpgDbContext"/> 类的新实例
+        /// <para>
+        /// 默认读取 XFrameworkConnString 配置里的连接串
+        /// </para>
+        /// </summary>
+        public NpgDbContext()
+            : base()
+        {
+        }
+
+        /// <summary>
+        /// 初始化 <see cref="NpgDbContext"/> 类的新实例
+        /// <param name="connString">数据库连接字符串</param>
+        /// </summary>
+        public NpgDbContext(string connString)
+            : this(connString, null)
+        {
+        }
+
+        /// <summary>
+        /// 初始化 <see cref="NpgDbContext"/> 类的新实例
+        /// <param name="connString">数据库连接字符串</param>
+        /// <param name="commandTimeout">执行命令超时时间</param>
+        /// </summary>
+        public NpgDbContext(string connString, int? commandTimeout)
+            : base(connString, commandTimeout)
+        {
+        }
+    }
+}
---
+++ src/Riz.XFramework.SQLite/Data/SqlClient/DbTypeUtils.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Data;
+using System.Data.SQLite;
+using System.Reflection;
+
+namespace Riz.XFramework.Data.SqlClient
+{
+    /// <summary>
+    /// 数据类型公用方法
+    /// </summary>
+    static class DbTypeUtils
+    {
+        /// <summary>
+        /// 设置命令参数对象的 DbType属性
+        /// </summary>
+        /// <param name="parameter">命令参数对象</param>
+        /// <param name="dbType">DbType属性</param>
+        public static void DbType(this SQLiteParameter parameter, object dbType)
+        {
+            if (dbType != null)
+            {
+                if (dbType is DbType)
+                    parameter.DbType = (DbType)dbType;
+                else
+                    DbTypeUtils.ThrowException(dbType);
+            }
+        }
+
+        /// <summary>
+        /// 是否时间类型
+        /// </summary>
+        /// <param name="dbType">DbType属性</param>
+        public static bool IsTime(object dbType)
+        {
+            if (dbType == null)
+                return false;
+            else if (dbType is DbType)
+                return ((DbType)dbType) == System.Data.DbType.Time;
+            else
+                return DbTypeUtils.ThrowException(dbType);
+        }
+
+        /// <summary>
+        /// 是否日期类型
+        /// </summary>
+        /// <param name="dbType">DbType属性</param>
+        public static bool IsDate(object dbType)
+        {
+            if (dbType == null)
+                return false;
+            else if (dbType is DbType)
+                return ((DbType)dbType) == System.Data.DbType.Date;
+            else
+                return DbTypeUtils.ThrowException(dbType);
+        }
+
+        /// <summary>
+        /// 是否日期+时间类型
+        /// </summary>
+        /// <param name="dbType">DbType属性</param>
+        public static bool IsDateTime(object dbType)
+        {
+            if (dbType == null)
+                return false;
+            else if (dbType is DbType)
+                return ((DbType)dbType) == System.Data.DbType.DateTime;
+            else
+                return DbTypeUtils.ThrowException(dbType);
+        }
+
+        /// <summary>
+        /// 是否日期+时间+精度类型
+        /// </summary>
+        /// <param name="dbType">DbType属性</param>
+        public static bool IsDateTime2(object dbType)
+        {
+            if (dbType == null)
+                return false;
+            else if (dbType is DbType)
+                return ((DbType)dbType) == System.Data.DbType.DateTime2;
+            else
+                return DbTypeUtils.ThrowException(dbType);
+        }
+
+        /// <summary>
+        /// 是否日期+时间+精度+时区类型
+        /// </summary>
+        /// <param name="dbType">DbType属性</param>
+        public static bool IsDateTimeOffset(object dbType)
+        {
+            if (dbType == null)
+                return false;
+            else if (dbType is DbType)
+                return ((DbType)dbType) == System.Data.DbType.DateTimeOffset;
+            else
+                return DbTypeUtils.ThrowException(dbType);
+        }
+
+        /// <summary>
+        /// 检查字段或属性成员声明的 DbType 是否为 Unicode 数据类型
+        /// </summary>
+        /// <param name="m">将要检查的字段或属性成员</param>
+        /// <returns></returns>
+        public static bool IsUnicode(MemberVisitedStack.VisitedMember m)
+        {
+            ColumnAttribute column = null;
+            return DbTypeUtils.IsUnicode(m, out column);
+        }
+
+        /// <summary>
+        /// 检查字段或属性成员声明的 DbType 是否为 Unicode 数据类型
+        /// </summary>
+        /// <param name="m">将要检查的字段或属性成员</param>
+        /// <param name="column">字段或属性成员显示声明的列特性</param>
+        /// <returns></returns>
+        public static bool IsUnicode(MemberVisitedStack.VisitedMember m, out ColumnAttribute column)
+        {
+            column = m != null ? TypeUtils.GetColumnAttribute(m.Member, m.ReflectedType) : null;
+            return DbTypeUtils.IsUnicode(column == null ? null : column.DbType);
+        }
+
+        /// <summary>
+        /// 检查是否Unicode数据类型
+        /// </summary>
+        /// <param name="dbType">DbType属性</param>
+        public static bool IsUnicode(object dbType)
+        {
+            return false;
+        }
+
+        // 抛出异常
+        static bool ThrowException(object dbType)
+        {
+            throw new NotSupportedException(string.Format("{0} is not a {1} type.", dbType, typeof(DbType).FullName));
+        }
+    }
+}
